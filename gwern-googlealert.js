148b017a9c0fc44dc3bfa8a6164cdab2e3eb65fc
Han Dao <hackerkiba@gmail.com>
2013-09-11 22:49:05 -0400

    create svg

--- gwern-googlealert.js
+++ gwern-googlealert.js
@@ -1,1 +1,11 @@
-var dataset = []
+var width = 900;
+var height = 500;
+var dataset = [];
+
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width",width)
+            .attr("height",height);
+
+console.log(d3.select("body"))
+
