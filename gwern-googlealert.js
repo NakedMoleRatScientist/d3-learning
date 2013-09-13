a79cd3955bb6260366cfc54c5ebe743163ac77a5
Han Dao <hackerkiba@gmail.com>
2013-09-12 21:38:04 -0400

    try to load json

--- gwern-googlealert.js
+++ gwern-googlealert.js
@@ -2,6 +2,7 @@ var width = 900;
 var height = 500;
 var dataset = [];
 
+var data = d3.json("googlealert.json")
 var svg = d3.select("body")
             .append("svg")
             .attr("width",width)
