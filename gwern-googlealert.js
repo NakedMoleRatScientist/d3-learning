65359b3b65dc33b99ac2084a14d8df77ed5c0128
Han Dao <hackerkiba@gmail.com>
2013-09-14 18:54:45 -0400

    move svg above

--- gwern-googlealert.js
+++ gwern-googlealert.js
@@ -2,6 +2,11 @@ var width = 900;
 var height = 500;
 var dataset = [];
 
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width",width)
+            .attr("height",height);
+
 d3.json("googlealert.json", function(data) {
   var xScale = d3.scale.linear();
   var dates = []
@@ -9,16 +14,12 @@ d3.json("googlealert.json", function(data) {
   {
     console.log(d3.time.format.parse(data[i]));
   }
-/*  var dateScale = d3.time.scale();
-  dataScale.domain(0,data.length);
-  dataScale.range(dates[0],dates[data.length -1]);
-*/
+  var timeScale = d3.time.scale();
+  timeScale.domain(0,data.length);
+  timeScale.range(dates[0],dates[data.length -1]);
 });
 
-var svg = d3.select("body")
-            .append("svg")
-            .attr("width",width)
-            .attr("height",height);
+
 
 
 
