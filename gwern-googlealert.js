a4eab98e33666fc2ecab7e396989c341efaec96d
Han Dao <hackerkiba@gmail.com>
2013-09-14 18:53:16 -0400

    add time scale

--- gwern-googlealert.js
+++ gwern-googlealert.js
@@ -2,11 +2,23 @@ var width = 900;
 var height = 500;
 var dataset = [];
 
-var data = d3.json("googlealert.json")
+d3.json("googlealert.json", function(data) {
+  var xScale = d3.scale.linear();
+  var dates = []
+  for (var i = 0; i < data.length; i++)
+  {
+    console.log(d3.time.format.parse(data[i]));
+  }
+/*  var dateScale = d3.time.scale();
+  dataScale.domain(0,data.length);
+  dataScale.range(dates[0],dates[data.length -1]);
+*/
+});
+
 var svg = d3.select("body")
             .append("svg")
             .attr("width",width)
             .attr("height",height);
 
-console.log(d3.select("body"))
+
 
