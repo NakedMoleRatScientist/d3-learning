92a4d40647bbef16ae182b76a836e200636e8bd2
Han Dao <hackerkiba@gmail.com>
2013-09-14 21:55:22 -0400

    draw circle

--- gwern-googlealert.js
+++ gwern-googlealert.js
@@ -18,8 +18,21 @@ d3.json("googlealert.json", function(data) {
   timeScale.domain([0,data.length]);
   timeScale.range(dates[0],dates[data.length - 1]);
   yScale = d3.scale.linear();
-  yScale.domain([0,d3.max(data, function(d) {return d.links})]);
+  yScale.domain([0,d3.max(data, function(d) {return parseInt(d.links);})]);
   yScale.range([height - padding, padding]);
+  svg.selectAll("circle")
+  .data(data)
+  .enter()
+  .append("circle")
+  .attr("cx", function(d,i)
+  {
+    return timeScale(dates[i]);
+  })
+  .attr("cy", function(d) {
+    return yScale(parseInt(d.links));
+  })
+  .attr("r", 2);
+
 });
 
 
