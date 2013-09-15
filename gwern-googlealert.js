03095dbc170487f7c09323014e0820d8f7961779
Han Dao <hackerkiba@gmail.com>
2013-09-14 20:08:56 -0400

    Figure out how to use d3 to parse date strings into actual dates and fixes a couple of problems afterward

--- gwern-googlealert.js
+++ gwern-googlealert.js
@@ -1,6 +1,5 @@
 var width = 900;
 var height = 500;
-var dataset = [];
 
 var svg = d3.select("body")
             .append("svg")
@@ -8,14 +7,17 @@ var svg = d3.select("body")
             .attr("height",height);
 
 d3.json("googlealert.json", function(data) {
-  var dates = []
+  var dates = [];
+  var format = d3.time.format("%Y-%m-%d");
   for (var i = 0; i < data.length; i++)
   {
-    console.log(d3.time.format.parse(data[i]));
+    dates.push(format.parse(data[i].date));
   }
   var timeScale = d3.time.scale();
-  timeScale.domain(0,data.length);
-  timeScale.range(dates[0],dates[data.length -1]);
+  timeScale.domain([0,data.length]);
+  timeScale.range(dates[0],dates[data.length - 1]);
+  yScale = d3.scale.linear()
+//  yScale.domain([0, d3.max(data, function(d) {return d[]})])
 });
 
 
