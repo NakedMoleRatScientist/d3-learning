751131962d8d2ddcd5c16e303224bc062b402d25
Han Dao <hackerkiba@gmail.com>
2013-09-17 16:54:18 -0400

    calculate the mean, q1, q3

--- boxplot.js
+++ boxplot.js
@@ -5,7 +5,9 @@ $(document).ready()
   var padding = 40;
   var dataset = [34, 34, 26, 37, 42, 41, 35, 31, 41, 33, 30, 74, 33, 49, 38, 61, 21, 41, 26, 80, 43, 29, 33, 35, 45, 49, 39, 34, 26, 25, 35, 33]
   dataset.sort()
-  median = (dataset[dataset.length / 2] + dataset[dataset.length / 2 + 1]) / 2
+  median = d3.median(dataset)
+  q1 = d3.quantile(dataset,0.25)
+  q3 = d3.quantile(dataset,0.75)
   var svg = d3.select("body")
     .append("svg")
     .attr("width",width)
