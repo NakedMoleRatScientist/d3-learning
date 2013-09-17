431a4607b905ecae37e7010e03d9efc009eedfd8
Han Dao <hackerkiba@gmail.com>
2013-09-17 16:34:58 -0400

    add dataset

--- boxplot.js
+++ boxplot.js
@@ -3,9 +3,10 @@ $(document).ready()
   var width = 800;
   var height = 600;
   var padding = 40;
-
+  var dataset = [34, 34, 26, 37, 42, 41, 35, 31, 41, 33, 30, 74, 33, 49, 38, 61, 21, 41, 26, 80, 43, 29, 33, 35, 45, 49, 39, 34, 26, 25, 35, 33]
+  
   var svg = d3.select("body")
     .append("svg")
     .attr("width",width)
-    .attr("height",height);
+    .attr("height",height)
 }
