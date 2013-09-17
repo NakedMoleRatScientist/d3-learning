c21f5992e2863f80d292cd5f2c6540bd88db98fc
Han Dao <hackerkiba@gmail.com>
2013-09-16 21:28:57 -0400

    add animation.js with initial setup

---
+++ animation.js
@@ -0,0 +1,10 @@
+var width = 800;
+var height = 600;
+
+
+var svg = d3.select("body")
+.append("svg")
+.attr("font-family","sans-serif")
+.attr("font-size","0.80em")
+.attr("width",width)
+.attr("height",height);
