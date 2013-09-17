b5eac32b924c78b83145e1cbee01a1a5fe6fe289
Han Dao <hackerkiba@gmail.com>
2013-09-16 22:17:02 -0400

    incorporate jquery

--- animation.js
+++ animation.js
@@ -1,10 +1,19 @@
-var width = 800;
-var height = 600;
+$(document).ready(function (){
+  var width = 800;
+  var height = 600;
 
 
-var svg = d3.select("body")
-.append("svg")
-.attr("font-family","sans-serif")
-.attr("font-size","0.80em")
-.attr("width",width)
-.attr("height",height);
+  var svg = d3.select("body")
+    .append("svg")
+    .attr("font-family","sans-serif")
+    .attr("font-size","0.80em")
+    .attr("width",width)
+    .attr("height",height);
+
+
+  var square = svg.append("rect")
+    .attr("x",90)
+    .attr("y",60)
+    .attr("width",60)
+    .attr('height',60);
+});  
