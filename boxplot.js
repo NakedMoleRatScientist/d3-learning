505193e01704ef38a59fb624d41531d1d3e68fce
Han Dao <hackerkiba@gmail.com>
2013-09-17 16:21:22 -0400

    create svg

--- boxplot.js
+++ boxplot.js
@@ -3,4 +3,9 @@ $(document).ready()
   var width = 800;
   var height = 600;
   var padding = 40;
+
+  var svg = d3.select("body")
+    .append("svg")
+    .attr("width",width)
+    .attr("height",height);
 }
