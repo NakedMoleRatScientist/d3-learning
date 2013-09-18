da3286afcd61f1d25193ba5d3d4a7dadd44e3994
Han Dao <hackerkiba@gmail.com>
2013-09-18 14:25:07 -0400

    add comment about what I am trying to do

--- boxplot.js
+++ boxplot.js
@@ -5,12 +5,15 @@ $(document).ready(function()
   var padding = 40;
   var dataset = [34, 34, 26, 37, 42, 41, 35, 31, 41, 33, 30, 74, 33, 49, 38, 61, 21, 41, 26, 80, 43, 29, 33, 35, 45, 49, 39, 34, 26, 25, 35, 33]
   dataset.sort()
+
+  //Claculate the various values needed for a boxplot.
   median = d3.median(dataset)
   q1 = d3.quantile(dataset,0.25)
   q3 = d3.quantile(dataset,0.75)
   iqr = q3 - q1
   bottom = q1 - 1.5 * iqr
   top = q3 + 1.5 * iqr
+  
   var svg = d3.select("body")
     .append("svg")
     .attr("width",width)
