1cbb5e83eed7c96d483843d241446f058b8a666e
Han Dao <hackerkiba@gmail.com>
2013-09-20 21:25:08 -0400

    add svg

--- linechart.js
+++ linechart.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
   var width = 800;
   var height = 600;
   console.log("Hello World");
+
+  var svg = d3.select("body")
+    .append("svg")
+    .attr("width",width)
+    .attr("height",height);
 });
