b1e684ffa723c7373924b71ba86bf51f2a2a23fe
Han Dao <hackerkiba@gmail.com>
2013-09-21 14:49:36 -0400

    add a dataset

--- linechart.js
+++ linechart.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
   var width = 800;
   var height = 600;
-  console.log("Hello World");
 
   var svg = d3.select("body")
     .append("svg")
     .attr("width",width)
     .attr("height",height);
+
+  dataset = [
+    ["2013-09-08", 3.04], ["2013-09-09", 3.17], ["2013-09-10",3.58],
+    ["2013-09-11", 2.21], ["2013-09-12", 4.24], ["2013-09-13",2.67],
+    ["2013-09-14",4.81]
+            ];
 });
