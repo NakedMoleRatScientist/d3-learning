0b5d5a253bacb3c6edb9c83eb71312fc64bba3cb
Han Dao <hackerkiba@gmail.com>
2013-09-21 15:02:09 -0400

    build a time scale

--- linechart.js
+++ linechart.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
   var width = 800;
   var height = 600;
-
+  var padding = 40;
   var svg = d3.select("body")
     .append("svg")
     .attr("width",width)
     .attr("height",height);
 
   var dataset = [
-    ["2013-09-08", 3.04], ["2013-09-09", 3.17], ["2013-09-10",3.58],
-    ["2013-09-11", 2.21], ["2013-09-12", 4.24], ["2013-09-13",2.67],
-    ["2013-09-14",4.81]
+    [new Date("2013-09-08"), 3.04], [new Date("2013-09-09"), 3.17], [new Date("2013-09-10"),3.58],
+    [new Date("2013-09-11"), 2.21], [new Date("2013-09-12"), 4.24], [new Date("2013-09-13"),2.67],
+    [new Date("2013-09-14"),4.81]
             ];
+
+  var xScale = d3.time.scale()
+  .domain([dataset[0][0], [dataset[dataset.length -1][0]]]);
+  .range(padding, width - padding);
+  
 });
