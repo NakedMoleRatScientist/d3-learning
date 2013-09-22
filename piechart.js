206f077c78a529e3c917b1dd8c97c142cd17aa18
Han Dao <hackerkiba@gmail.com>
2013-09-22 18:39:09 -0400

    add initial code

---
+++ piechart.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  var width = 800;
+  var height = 600;
+  var padding = 40;
+  var svg = d3.select("body")
+  .append("svg")
+  .attr("width",width)
+  .attr("height",height);
+
+  var data = [33,40,30];
+  
+});
