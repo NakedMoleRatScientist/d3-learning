095ac083cd2b551b19c4f2fab427be14f8f7fc86
Han Dao <hackerkiba@gmail.com>
2013-09-22 19:26:24 -0400

    add data

---
+++ heatmap.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  var width = 800;
+  var height = 600;
+  var padding = 40;
+
+  var data =
+    [
+      [2,0,4,3,3,5,7],
+      [2,3,1,1,1,1,1],
+      [4,3,5,2,3,5,6],
+      [8,3,3,3,3,3,2]
+    ];
+});
