f1f2ccea774b1e39770d2094638d1579db0d500b
Han Dao <hackerkiba@gmail.com>
2013-09-22 19:27:44 -0400

    add a range of colors to work with

--- heatmap.js
+++ heatmap.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   var height = 600;
   var padding = 40;
 
+  var colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"];
   var data =
     [
       [2,0,4,3,3,5,7],
@@ -10,4 +11,6 @@ $(document).ready(function() {
       [4,3,5,2,3,5,6],
       [8,3,3,3,3,3,2]
     ];
+
+  
 });
